73a2e8858c140631ba0d7a3d83129b5233a8a726
Philip Wisner <philipwisner@gmail.com>
2017-08-12 16:22:14 +0200

    Updated version with prompt as function and rover paramenter. Still no wrapping

--- ih-prework-html-css/mars_rover/rover.js
+++ ih-prework-html-css/mars_rover/rover.js
@@ -7,8 +7,6 @@ var myRover = {
 //Create a 10 x 10 grid for the rover
 var planetGrid = [[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9]];
 
-var maxGridX = planetGrid[0][10];
-var minGridY = planetGrid[1][10];
 
 //Prompt for input of directions - Make it accept a string
 var promptIntro = "Move the rover by typing the following commands:\n f = Forward\n b = Backwards\n l = Left\n r = Right ";
@@ -91,32 +89,37 @@ function rightTurn(rover) {
 };
 
 
-// Checking to see if prompt registers proper entry
+// Prompt to input directions and then run
+function promptMovement(rover) {
 var inputMoves = window.prompt(promptIntro);
 	for (var i = 0; i < inputMoves.length; i++) {
-		switch (inputMoves[i]) {
+		switch (inputMoves[i].toLowerCase()) {
 			case 'f':
-			goForward(myRover);
+			goForward(rover);
 			console.log('You moved forward');
 			break;
 			case 'b':
-			goBackward(myRover);
+			goBackward(rover);
 			console.log('You moved backward');
 			break;
 			case 'l':
-			leftTurn(myRover);
-			console.log('You moved left');
+			leftTurn(rover);
+			console.log('You turned left');
 			break;
 			case 'r':
-			rightTurn(myRover);
-			console.log('You moved right');
+			rightTurn(rover);
+			console.log('You turned right');
 			break;
 			default:
 				console.log('Invalid input');
 		}
-		console.log("The rover is currently at: [" + myRover.position[0] + ", " + myRover.position[1] +"]" + " and facing " + myRover.direction);
 	}
+//Output the rover's final position
+console.log("The rover is currently at: [" + myRover.position[0] + ", " + myRover.position[1] +"]" + " and facing " + myRover.direction);
+}
 
+//Run the function with object myRover
+promptMovement(myRover);
 
 
 /* Function for just rover moves - NO PROMPT
@@ -149,5 +152,18 @@ function roverMove(inputMoves) {
 
 //roverMove('fffrfflfffbb');
 
+/*
 
-
+		if (rover.position[0] = 10) {
+			rover.positon[0] = 0;
+		}
+		else if (rover.position[0] <= -1) {
+			rover.position[0] = 10;
+		}
+		else if (rover.position[1] >= 10) {
+			rover.position[1] = 0;
+		}
+		else if (myRover.position[1] <= -1) {
+			rover.position[1] = 10;
+		};
+*/
